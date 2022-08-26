9b2d7f58998b47c6a8b81fcd6f2619fb7534aa79
matej <matejb6@icloud.com>
2022-08-26 21:02:06 +0200

    Add nav buttons module

---
+++ src/app/layout/nav-buttons/nav-buttons.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import { appRoutes } from 'src/app/app-routes';
+
+@Component({
+  selector: 'app-nav-buttons',
+  templateUrl: './nav-buttons.component.html',
+  styleUrls: ['./nav-buttons.component.scss']
+})
+export class NavButtonsComponent implements OnInit {
+  public routes: string[] = [];
+
+  ngOnInit() {
+    this.routes = this.getRoutesFromObject(appRoutes);
+  }
+
+  private getRoutesFromObject(obj: any): string[] {
+    return Object.values(obj);
+  }
+}
