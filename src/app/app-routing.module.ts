f8fb47ad0609843caa8fe5a79d9a197d48467a5c
matej <matejb6@icloud.com>
2022-08-26 17:28:19 +0200

    Add players view route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'players',
+    title: 'Table Tennis Results Tracker • Players',
+    loadChildren: () => import('./views/players/players.module').then((m) => m.PlayersModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
