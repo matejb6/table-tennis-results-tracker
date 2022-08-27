289548b8483e6000b29591b75de1aab420d54c97
matej <matejb6@icloud.com>
2022-08-27 21:47:34 +0200

    Fix add player dialog tests

--- src/app/shared/components/add-player-dialog/add-player-dialog.component.spec.ts
+++ src/app/shared/components/add-player-dialog/add-player-dialog.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogRef } from '@angular/material/dialog';
 
 import { AddPlayerDialogModule } from './add-player-dialog.module';
 import { AddPlayerDialogComponent } from './add-player-dialog.component';
@@ -11,7 +12,8 @@ describe('AddPlayerDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [BrowserAnimationsModule, AddPlayerDialogModule]
+      imports: [BrowserAnimationsModule, AddPlayerDialogModule],
+      providers: [{ provide: MatDialogRef, useValue: {} }]
     }).compileComponents();
 
     fixture = TestBed.createComponent(AddPlayerDialogComponent);
