9ee392c05f779b8e4f669d55534dab51342edecc
matej <matejb6@icloud.com>
2022-08-27 21:54:02 +0200

    Parse columns from data source in table component

--- src/app/shared/components/table/table.component.ts
+++ src/app/shared/components/table/table.component.ts
@@ -1,26 +1,35 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
-export class TableComponent<T> implements OnInit {
-  private readonly positionKey: string = 'pos';
-  @Input()
-  public displayedColumns: string[] = [];
+export class TableComponent<T> implements OnInit, OnChanges {
+  private readonly positionKey: string = 'position';
   @Input()
   public dataSource: T[] = [];
+  public columns: string[] = [];
 
   constructor() {}
 
   ngOnInit() {
+    this.initColumns();
     this.addPositionColumn();
     this.addPositionToDataSource();
   }
 
+  ngOnChanges() {
+    this.addPositionColumn();
+    this.addPositionToDataSource();
+  }
+
+  private initColumns(): void {
+    this.columns = Object.keys(this.dataSource[0]);
+  }
+
   private addPositionColumn(): void {
-    this.displayedColumns.unshift(this.positionKey);
+    !this.columns.includes(this.positionKey) && this.columns.unshift(this.positionKey);
   }
 
   private addPositionToDataSource(): void {
