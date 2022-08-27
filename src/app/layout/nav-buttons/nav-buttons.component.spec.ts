49e7aa8ff65d6cfa6d597ac37f06804d03b38ffe
matej <matejb6@icloud.com>
2022-08-27 10:29:36 +0200

    Remove unused import

--- src/app/layout/nav-buttons/nav-buttons.component.spec.ts
+++ src/app/layout/nav-buttons/nav-buttons.component.spec.ts
@@ -1,7 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
-import { NavButtonsModule } from './nav-buttons.module';
 import { NavButtonsComponent } from './nav-buttons.component';
 
 describe('NavButtonsComponent', () => {
