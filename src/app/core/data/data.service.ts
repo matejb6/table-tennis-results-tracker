9a5d92f65dbb57b3306820d99f8715bfffbf3c16
matej <matejb6@icloud.com>
2022-08-27 12:50:57 +0200

    Extend data service with players initial data

--- src/app/core/data/data.service.ts
+++ src/app/core/data/data.service.ts
@@ -1,6 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Player } from '@core/models/player';
 
 @Injectable({
   providedIn: 'root'
 })
-export class DataService {}
+export class DataService {
+  private players: Player[] = [
+    { position: 1, name: 'Lucija', setsWon: 10 },
+    { position: 2, name: 'Antun', setsWon: 8 },
+    { position: 3, name: 'Matej', setsWon: 5 }
+  ];
+
+  public getPlayers(): Player[] {
+    return this.players;
+  }
+}
