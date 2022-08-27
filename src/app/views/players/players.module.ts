9acd8b6c69927bc58786965b3ea28f9539bbac13
matej <matejb6@icloud.com>
2022-08-27 13:06:06 +0200

    Add add player button, introduce title bar

--- src/app/views/players/players.module.ts
+++ src/app/views/players/players.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 import { SharedModule } from '@shared/shared.module';
 import { PlayersRoutingModule } from './players-routing.module';
 import { PlayersComponent } from './players.component';
 
 @NgModule({
-  imports: [SharedModule, PlayersRoutingModule],
+  imports: [MatButtonModule, SharedModule, PlayersRoutingModule],
   exports: [],
   declarations: [PlayersComponent],
   providers: []
