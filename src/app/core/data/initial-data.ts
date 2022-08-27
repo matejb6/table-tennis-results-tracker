e43f339aeb92c06ff843e77689f64b836457015e
matej <matejb6@icloud.com>
2022-08-27 21:46:26 +0200

    Add initial data

---
+++ src/app/core/data/initial-data.ts
@@ -0,0 +1,7 @@
+import { Player } from '@core/models/player';
+
+export const players: Player[] = [
+  { name: 'Lucija', setsWon: 10 },
+  { name: 'Antun', setsWon: 8 },
+  { name: 'Matej', setsWon: 5 }
+];
