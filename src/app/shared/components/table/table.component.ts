17f5ad0029e350deb7966d169d1187d4509d0b14
matej <matejb6@icloud.com>
2022-08-27 14:11:32 +0200

    Auto create position column in table component

--- src/app/shared/components/table/table.component.ts
+++ src/app/shared/components/table/table.component.ts
@@ -1,13 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
-export class TableComponent<T> {
+export class TableComponent<T> implements OnInit {
+  private readonly positionKey: string = 'pos';
   @Input()
   public displayedColumns: string[] = [];
   @Input()
   public dataSource: T[] = [];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.addPositionColumn();
+    this.addPositionToDataSource();
+  }
+
+  private addPositionColumn(): void {
+    this.displayedColumns.unshift(this.positionKey);
+  }
+
+  private addPositionToDataSource(): void {
+    this.dataSource.forEach((item, index) => {
+      Object.defineProperty(item, this.positionKey, {
+        value: index + 1
+      });
+    });
+  }
 }
