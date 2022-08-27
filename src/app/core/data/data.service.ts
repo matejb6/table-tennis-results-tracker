355ae80f3a4ce9110d24686235df7d19cbb1d4e2
matej <matejb6@icloud.com>
2022-08-27 21:46:42 +0200

    Refactor data service with observables

--- src/app/core/data/data.service.ts
+++ src/app/core/data/data.service.ts
@@ -1,18 +1,33 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 import { Player } from '@core/models/player';
+import { players } from './initial-data';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-  private players: Player[] = [
-    { name: 'Lucija', setsWon: 10 },
-    { name: 'Antun', setsWon: 8 },
-    { name: 'Matej', setsWon: 5 }
-  ];
+  private playersBehaviorSubject: BehaviorSubject<Player[]> = new BehaviorSubject(players);
+  public $playersObs: Observable<Player[]> = this.playersBehaviorSubject.asObservable();
 
-  public getPlayers(): Player[] {
-    return this.players;
+  constructor() {}
+
+  /**
+   * @returns Players observable
+   * @description Returns players behavior subject as observable
+   */
+  public getPlayersObs(): Observable<Player[]> {
+    return this.playersBehaviorSubject.asObservable();
+  }
+
+  /**
+   * @param name: Name
+   * @description Adds player by emitting new data with behavior subject
+   */
+  public addPlayer(name: string): void {
+    const values = this.playersBehaviorSubject.getValue();
+    values.push({ name: name, setsWon: 0 });
+    this.playersBehaviorSubject.next([...values]);
   }
 }
