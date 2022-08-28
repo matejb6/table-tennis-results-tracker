8ebe08e3350a06049e25102a54c594dcafc6904d
matej <matejb6@icloud.com>
2022-08-28 12:40:15 +0200

    Remove position column creation from table component

--- src/app/shared/components/table/table.component.ts
+++ src/app/shared/components/table/table.component.ts
@@ -1,13 +1,11 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
-export class TableComponent<T> implements OnInit, OnChanges {
-  private readonly positionKey: string = 'position';
-
+export class TableComponent<T> implements OnInit {
   @Input()
   public dataSource: T[] = [];
   @Output()
@@ -18,29 +16,10 @@ export class TableComponent<T> implements OnInit, OnChanges {
 
   ngOnInit() {
     this.initColumns();
-    this.addPositionColumn();
-    this.addPositionToDataSource();
-  }
-
-  ngOnChanges() {
-    this.addPositionColumn();
-    this.addPositionToDataSource();
   }
 
   private initColumns(): void {
     this.columns = this.dataSource.length ? Object.keys(this.dataSource[0]) : [];
-  }
-
-  private addPositionColumn(): void {
-    !this.columns.includes(this.positionKey) && this.columns.unshift(this.positionKey);
-  }
-
-  private addPositionToDataSource(): void {
-    this.dataSource.forEach((item, index) => {
-      Object.defineProperty(item, this.positionKey, {
-        value: index + 1
-      });
-    });
   }
 
   public onRowClick(row: T): void {
