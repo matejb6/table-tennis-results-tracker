0c838272b837ca2257309178a5eabf3ca56cccb4
matej <matejb6@icloud.com>
2022-08-28 10:10:32 +0200

    Add player on form submit and refresh table

--- src/app/views/players/players.component.ts
+++ src/app/views/players/players.component.ts
@@ -1,7 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
 
 import { DataService } from '@core/data/data.service';
 import { Player } from '@core/models/player';
+import {
+  AddPlayerDialogComponent,
+  AddPlayerForm
+} from '@shared/components/add-player-dialog/add-player-dialog.component';
+import { DialogService } from '@shared/services/dialog/dialog.service';
 
 @Component({
   selector: 'app-players',
@@ -9,13 +16,28 @@ import { Player } from '@core/models/player';
   styleUrls: ['./players.component.scss']
 })
 export class PlayersComponent implements OnInit {
-  displayedColumns: string[] = [];
-  dataSource: Player[] = [];
+  public $players: Observable<Player[]> = new Observable<Player[]>();
 
-  constructor(private dataService: DataService) {}
+  constructor(private dataService: DataService, private dialogService: DialogService) {}
 
   ngOnInit(): void {
-    this.displayedColumns = Object.keys(this.dataService.getPlayers()[0]);
-    this.dataSource = this.dataService.getPlayers();
+    this.initTable();
+  }
+
+  private initTable(): void {
+    this.$players = this.dataService.getPlayersObs();
+  }
+
+  public onAddPlayerClick(): void {
+    const dialogRef = this.dialogService.openDialog(AddPlayerDialogComponent) as MatDialogRef<
+      AddPlayerDialogComponent,
+      AddPlayerForm
+    >;
+
+    dialogRef.afterClosed().subscribe({
+      next: (value) => {
+        value && this.dataService.addPlayer(value?.name);
+      }
+    });
   }
 }
--- src/app/views/players/players.module.ts
+++ src/app/views/players/players.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 
 import { SharedModule } from '@shared/shared.module';
@@ -6,7 +7,7 @@ import { PlayersRoutingModule } from './players-routing.module';
 import { PlayersComponent } from './players.component';
 
 @NgModule({
-  imports: [MatButtonModule, SharedModule, PlayersRoutingModule],
+  imports: [CommonModule, MatButtonModule, SharedModule, PlayersRoutingModule],
   exports: [],
   declarations: [PlayersComponent],
   providers: []
