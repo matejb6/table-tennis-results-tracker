44b1c56835a9208306ea33a6e7afdef87743365e
matej <matejb6@icloud.com>
2022-08-28 12:38:39 +0200

    Add position to initial data

--- src/app/core/data/initial-data.ts
+++ src/app/core/data/initial-data.ts
@@ -1,7 +1,7 @@
 import { Player } from '@core/models/player';
 
 export const players: Player[] = [
-  { name: 'Antun', setsWon: 8 },
-  { name: 'Lucija', setsWon: 10 },
-  { name: 'Matej', setsWon: 5 }
+  { position: 2, name: 'Antun', setsWon: 8 },
+  { position: 1, name: 'Lucija', setsWon: 10 },
+  { position: 3, name: 'Matej', setsWon: 5 }
 ];
