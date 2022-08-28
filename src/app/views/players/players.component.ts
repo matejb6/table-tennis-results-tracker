bd6b2f4a9047028854ee262ec8de1b7ae04f30d2
matej <matejb6@icloud.com>
2022-08-28 11:13:11 +0200

    Add check for existing player when adding new one

--- src/app/views/players/players.component.ts
+++ src/app/views/players/players.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
-import { Observable } from 'rxjs';
+import { firstValueFrom, Observable } from 'rxjs';
 
 import { DataService } from '@core/data/data.service';
 import { Player } from '@core/models/player';
@@ -9,6 +9,7 @@ import {
   AddPlayerForm
 } from '@shared/components/add-player-dialog/add-player-dialog.component';
 import { DialogService } from '@shared/services/dialog/dialog.service';
+import { SnackBarService } from '@shared/services/snack-bar/snack-bar.service';
 
 @Component({
   selector: 'app-players',
@@ -18,26 +19,40 @@ import { DialogService } from '@shared/services/dialog/dialog.service';
 export class PlayersComponent implements OnInit {
   public $players: Observable<Player[]> = new Observable<Player[]>();
 
-  constructor(private dataService: DataService, private dialogService: DialogService) {}
+  constructor(
+    private dataService: DataService,
+    private dialogService: DialogService,
+    private snackBarService: SnackBarService
+  ) {}
 
   ngOnInit(): void {
-    this.initTable();
-  }
-
-  private initTable(): void {
     this.$players = this.dataService.getPlayersObs();
   }
 
-  public onAddPlayerClick(): void {
+  private async getPlayers(): Promise<Player[]> {
+    return await firstValueFrom(this.dataService.getPlayersObs());
+  }
+
+  private newPlayerExists(players: Player[], newPlayerName: string): boolean {
+    return players.some((item) => item.name === newPlayerName);
+  }
+
+  public async onAddPlayerClick(): Promise<void> {
     const dialogRef = this.dialogService.openDialog(AddPlayerDialogComponent) as MatDialogRef<
       AddPlayerDialogComponent,
       AddPlayerForm
     >;
 
-    dialogRef.afterClosed().subscribe({
-      next: (value) => {
-        value && this.dataService.addPlayer(value?.name);
-      }
-    });
+    const players = await this.getPlayers();
+    const newPlayer = await firstValueFrom(dialogRef.afterClosed());
+
+    const newPlayerExists = this.newPlayerExists(players, newPlayer?.name!);
+
+    if (!newPlayerExists) {
+      this.dataService.addPlayer(newPlayer?.name!);
+      this.snackBarService.showSnackBar('Player added');
+    } else {
+      this.snackBarService.showSnackBar('Player already exists');
+    }
   }
 }
