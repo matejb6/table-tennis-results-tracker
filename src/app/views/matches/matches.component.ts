b038b7578bc96b333badc3ad736292944da8f7bb
matej <matejb6@icloud.com>
2022-08-29 07:59:06 +0200

    Add add match dialog to matches view

--- src/app/views/matches/matches.component.ts
+++ src/app/views/matches/matches.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+
+import { DataService } from '@core/data/data.service';
+import { DialogService } from '@shared/services/dialog/dialog.service';
+import { AddMatchDialogComponent } from '@shared/components/add-match-dialog/add-match-dialog.component';
 
 @Component({
   selector: 'app-matches',
   templateUrl: './matches.component.html',
   styleUrls: ['./matches.component.scss']
 })
-export class MatchesComponent {}
+export class MatchesComponent {
+  constructor(private dataService: DataService, private dialogService: DialogService) {}
+
+  public async onAddMatchClick(): Promise<void> {
+    const players = await firstValueFrom(this.dataService.getPlayersObs());
+    this.dialogService.openDialog(AddMatchDialogComponent, players);
+  }
+}
