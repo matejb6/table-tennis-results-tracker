988ebbe3b0ae6a526661695cbabc305e8e65ddf1
matej <matejb6@icloud.com>
2022-08-29 19:03:42 +0200

    Add matches initial data

--- src/app/core/data/initial-data.ts
+++ src/app/core/data/initial-data.ts
@@ -1,7 +1,32 @@
+import { Match } from '@core/models/match';
 import { Player } from '@core/models/player';
 
 export const players: Player[] = [
-  { name: 'Antun', setsWon: 4 },
-  { name: 'Lucija', setsWon: 9 },
-  { name: 'Matej', setsWon: 2 }
+  { name: 'Antun', setsWon: 1 },
+  { name: 'Lucija', setsWon: 3 },
+  { name: 'Matej', setsWon: 0 }
+];
+
+export const matches: Match[] = [
+  {
+    players: ['Lucija', 'Antun'],
+    sets: [
+      {
+        firstPlayerScore: 11,
+        secondPlayerScore: 9
+      },
+      {
+        firstPlayerScore: 10,
+        secondPlayerScore: 12
+      },
+      {
+        firstPlayerScore: 12,
+        secondPlayerScore: 10
+      },
+      {
+        firstPlayerScore: 11,
+        secondPlayerScore: 8
+      }
+    ]
+  }
 ];
