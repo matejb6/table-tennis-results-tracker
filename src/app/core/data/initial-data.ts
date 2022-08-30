6cbe1e196c5306a084a4908447aade71014606c2
matej <matejb6@icloud.com>
2022-08-30 11:26:03 +0200

    Update initial data

--- src/app/core/data/initial-data.ts
+++ src/app/core/data/initial-data.ts
@@ -2,31 +2,23 @@ import { Match } from '@core/models/match';
 import { Player } from '@core/models/player';
 
 export const players: Player[] = [
-  { name: 'Antun', setsWon: 1 },
-  { name: 'Lucija', setsWon: 3 },
-  { name: 'Matej', setsWon: 0 }
+  { id: 1660986000000, name: 'Antun', matchesPlayed: 1, matchesWon: 0, setsWon: 1 },
+  { id: 1660982400000, name: 'Lucija', matchesPlayed: 1, matchesWon: 1, setsWon: 3 },
+  { id: 1660989600000, name: 'Matej', matchesPlayed: 0, matchesWon: 0, setsWon: 0 }
 ];
 
 export const matches: Match[] = [
   {
-    players: ['Lucija', 'Antun'],
+    id: 1661068800000,
+    players: [players[1], players[0]],
     sets: [
-      {
-        firstPlayerScore: 11,
-        secondPlayerScore: 9
-      },
-      {
-        firstPlayerScore: 10,
-        secondPlayerScore: 12
-      },
-      {
-        firstPlayerScore: 12,
-        secondPlayerScore: 10
-      },
-      {
-        firstPlayerScore: 11,
-        secondPlayerScore: 8
-      }
-    ]
+      [11, 9],
+      [10, 12],
+      [12, 10],
+      [11, 8]
+    ],
+    score: [3, 1],
+    winner: players[1],
+    date: new Date(1661068800000).toUTCString()
   }
 ];
