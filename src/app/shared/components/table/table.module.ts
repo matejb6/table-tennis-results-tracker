800452d8b1a2e537e9569cf4ae0384dbe4e2695c
matej <matejb6@icloud.com>
2022-09-09 10:40:31 +0200

    Transform table column labels with pipes

--- src/app/shared/components/table/table.module.ts
+++ src/app/shared/components/table/table.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 
+import { PipesModule } from '@shared/pipes/pipes.module';
 import { TableComponent } from './table.component';
 
 @NgModule({
-  imports: [CommonModule, MatTableModule],
+  imports: [CommonModule, MatTableModule, PipesModule],
   exports: [TableComponent],
   declarations: [TableComponent],
   providers: []
