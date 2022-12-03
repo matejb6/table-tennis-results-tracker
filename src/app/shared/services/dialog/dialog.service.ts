811b04af9ba9b2d535111239d57bcfd42ed1708c
Matej Buljan <matejb6@icloud.com>
2022-12-03 14:27:49 +0100

    Refactor dialog service

--- src/app/shared/services/dialog/dialog.service.ts
+++ src/app/shared/services/dialog/dialog.service.ts
@@ -1,28 +1,21 @@
 import { Injectable } from '@angular/core';
-import { MatDialog, MatDialogConfig, MatDialogRef } from '@angular/material/dialog';
+import { ComponentType } from '@angular/cdk/overlay';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 
 @Injectable()
 export class DialogService {
   constructor(private dialog: MatDialog) {}
 
   /**
-   * Returns dialog config
-   * @param data Data
-   * @returns Dialog config
-   */
-  private getDialogConfig(data?: any): MatDialogConfig {
-    return {
-      data: data
-    };
-  }
-
-  /**
-   * Opens dialog with component and injects data
+   * Opens dialog with component and injects data,
+   * where T is component type, U is type of dialog result and V is data type
    * @param component Component to open
    * @param data Data to inject
    * @returns Dialog reference
    */
-  public openDialog(component: any, data?: any): MatDialogRef<unknown, any> {
-    return this.dialog.open(component, this.getDialogConfig(data));
+  public openDialog<T, U, V>(component: ComponentType<T>, data?: V): MatDialogRef<T, U> {
+    return this.dialog.open(component, {
+      data: data
+    });
   }
 }
