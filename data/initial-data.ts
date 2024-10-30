6ebcc70b7c08dcd5aea2289b3932b57b5b08f280
Matej Buljan <matejb6@icloud.com>
2024-10-30 10:49:32 +0100

    Move initial data to root folder

--- data/initial-data.ts
+++ data/initial-data.ts
@@ -1,16 +1,16 @@
 import { Match } from '@core/models/match';
 import { Player } from '@core/models/player';
 
-export const players: Player[] = [
+export const PLAYERS: Player[] = [
   { id: 1660986000000, name: 'Antun', matchesPlayed: 1, matchesWon: 0, setsWon: 1 },
   { id: 1660982400000, name: 'Lucija', matchesPlayed: 1, matchesWon: 1, setsWon: 3 },
   { id: 1660989600000, name: 'Matej', matchesPlayed: 0, matchesWon: 0, setsWon: 0 }
 ];
 
-export const matches: Match[] = [
+export const MATCHES: Match[] = [
   {
     id: 1661068800000,
-    players: [players[1], players[0]],
+    players: [PLAYERS[1], PLAYERS[0]],
     sets: [
       [11, 9],
       [10, 12],
@@ -18,7 +18,7 @@ export const matches: Match[] = [
       [11, 8]
     ],
     score: [3, 1],
-    winner: players[1],
+    winner: PLAYERS[1],
     date: new Date(1661068800000).toUTCString()
   }
 ];
