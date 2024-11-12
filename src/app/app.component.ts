06d8fde2de8c73c57a46cf837666cf794f05c2e7
Matej Buljan <matejb6@icloud.com>
2024-11-12 11:02:52 +0100

    Change app bootstrapping, app module to app component, routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { CoreModule } from '@core/core.module';
+import { LayoutModule } from '@layout/layout.module';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CoreModule, LayoutModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
